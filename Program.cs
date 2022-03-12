dff35ce7cbaff0c80cfc5c384fd3d1a4ec98a023
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-03-12 16:20:00 +0300

    Move up

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+await Bootstrapper
+        .Factory
+        .CreateWeb(args)
+        .DeployToGitHubPages(
+            "yapoml",
+            "yapoml.github.io",
+            Config.FromSetting<string>("GITHUB_TOKEN"),
+            "gh-pages"
+    )
+        .RunAsync();
--- src/Yapoml.Documentation/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Statiq.App;
-using Statiq.Web;
-
-await Bootstrapper
-        .Factory
-        .CreateWeb(args)
-        .RunAsync();
